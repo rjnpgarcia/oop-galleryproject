08f7cf3a2ea9ff11f1a5c04a535b3e2132e5c200
rjnpgarcia <102405417+rjnpgarcia@users.noreply.github.com>
2022-10-06 21:10:31 +0800

    Transferred gallery codes and files

---
+++ admin/includes/db.php
@@ -0,0 +1,10 @@
+<?php
+
+// Database Connection
+const DB_HOST = "localhost";
+const DB_USER = "root";
+const DB_PASS = "";
+const DB_NAME = "gallery_db";
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+#######################
---
+++ admin/includes/sidenav.php
@@ -0,0 +1,19 @@
+<div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav side-nav">
+        <li>
+            <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+        </li>
+        <li>
+            <a href="admin_users.php"><i class="fa fa-fw fa-users"></i> Users</a>
+        </li>
+        <li>
+            <a href="admin_upload.php"><i class="fa fa-fw fa-upload"></i> Upload</a>
+        </li>
+        <li>
+            <a href="admin_photos.php"><i class="fa fa-fw fa-image"></i> Photos</a>
+        </li>
+        <li>
+            <a href="admin_comments.php"><i class="fa fa-fw fa-comment"></i> Comments</a>
+        </li>
+    </ul>
+</div>
